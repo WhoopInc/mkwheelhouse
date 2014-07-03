1a2c0f536155301bf2f337af73539562746f5c48
Nikhil Benesch <nikhil.benesch@gmail.com>
2014-07-02 21:20:29 -0400

    setup.py: add classifiers

--- setup.py
+++ setup.py
@@ -8,6 +8,13 @@ setup(
     py_modules=['mkwheelhouse'],
     url='https://github.com/WhoopInc/mkwheelhouse',
     description='Amazon S3 wheelhouse generator',
+    classifiers=[
+      'License :: OSI Approved :: MIT License',
+      'Development Status :: 4 - Beta',
+      'Environment :: Console',
+      'Intended Audience :: Developers',
+      'Topic :: Software Development :: Build Tools'
+    ],
     install_requires=[
         "awscli >= 1.3.6",
         "yattag >= 0.9.2",
