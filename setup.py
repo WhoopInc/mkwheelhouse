399e325231655d060aa07790998af616f7a4dd1e
Nikhil Benesch <nikhil.benesch@gmail.com>
2014-07-02 21:53:42 -0400

    release: 0.2.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='mkwheelhouse',
-    version='0.2.0',
+    version='0.2.1',
     author='Nikhil Benesch',
     author_email='benesch@whoop.com',
     py_modules=['mkwheelhouse'],
